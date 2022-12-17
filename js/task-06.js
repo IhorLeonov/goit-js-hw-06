a7be00f7fc32a4f3f04f0f9280032426117fa11f
Ihor Leonov <ihorleonow@gmail.com>
2022-12-17 21:06:44 +0200

    Upd task 6

--- js/task-06.js
+++ js/task-06.js
@@ -7,12 +7,19 @@ input.addEventListener('blur', inputOnBlur);
 function inputOnBlur(event) {
   input.classList.add('invalid');
 
+  // if (event.currentTarget.value.length == lengthEl) {
+  //   input.classList.add('valid');
+  //   if (input.classList.contains('invalid')) {
+  //     input.classList.remove('invalid');
+  //   }
+  // } else if (input.classList.contains('valid')) {
+  //   input.classList.remove('valid');
+  // }
+
   if (event.currentTarget.value.length == lengthEl) {
     input.classList.add('valid');
-    if (input.classList.contains('invalid')) {
-      input.classList.remove('invalid');
-    }
-  } else if (input.classList.contains('valid')) {
+    input.classList.remove('invalid');
+  } else {
     input.classList.remove('valid');
   }
 }
